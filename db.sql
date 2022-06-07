56137cfd8d2c703283558a53b68172856f2a6e89
Ali Jimmy <hereisjimmymartin@gmail.com>
2022-06-08 00:18:20 +0430

    docker compose file and example sql file created

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE accounts (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO accounts(username, password, email) VALUES ('admin', 'admin', 'admin@email.com');
